a531fc9aef54ebc618d7ed1000feea4fdb3fa00e
Beatriz Bittencourt <beatrizbittencourtdealmeida@gmail.com>
2024-05-14 11:46:42 -0300

    atualização página do filme

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -10,7 +10,7 @@ export default async function MoviePage({ params }: { params: { id: string } }) 
 
   return (
     <div className="w-full">
-      <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx:auto md:space-x-6">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
         <Image
           src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`}
           width={500}
@@ -19,11 +19,15 @@ export default async function MoviePage({ params }: { params: { id: string } }) 
           className="rounded-lg"
           style={{ maxWidth: '100%', height: '100%' }}
         />
-        <div className="p-2">
-          <h2>{movie.title || movie.name}</h2>
-          <p>{movie.overview}</p>
-          <p>{movie.release_date || movie.first_air_date}</p>
-          <p>{movie.vote_count}</p>
+        <div className="p-2 text-justify">
+          <h2 className='text-lg mb-3 font-bold'>{movie.title || movie.name}</h2>
+          <p className='text-md mb-3'>{movie.overview}</p>
+          <p className='mb-3'>
+            <span className='font-semibold mr-1'>Data de lançamento: </span>
+            {movie.release_date || movie.first_air_date}</p>
+          <p>
+          <span className='font-semibold mr-1'>Avaliações: </span>
+            {movie.vote_count}</p>
         </div>
       </div>
     </div>
